91b2af67d9ea9cb91ecc30a4f0dea37315bbae92
Robinkim93 <Robinkim93@gmail.com>
2023-03-07 16:56:09 +0900

    data binding

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <input type="text" v-model="name" />
+  <button class="btn btn-primary" @click="onSubmit">Click</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from "vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const name = ref("Robin");
+
+    const onSubmit = () => {
+      console.log(name.value);
+    };
+
+    return { name, onSubmit };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.name {
+  color: red;
 }
 </style>
